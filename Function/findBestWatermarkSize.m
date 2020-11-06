ceb473254015be78b3edef107c092e70b708efcf
魁の光 <haidangtran97@gmail.com>
2020-11-06 22:39:25 +0900

    Official release v1.0

--- Function/findBestWatermarkSize.m
+++ Function/findBestWatermarkSize.m
@@ -1,4 +1,4 @@
-function AttackedWatermarkedImage = resizeAtk(WatermarkedImagePath, ratio)
+function [WatermarkH,WatermarkW] = findBestWatermarkSize(HostImagePath)
 % ______             ______            
 % | ___ \            |  _  \           
 % | |_/ / __ _  ___  | | | |__ _ _   _ 
@@ -14,22 +14,24 @@ function AttackedWatermarkedImage = resizeAtk(WatermarkedImagePath, ratio)
 % #          Email - haidangtran97@gmail.com                        #
 % ###################################################################
 
-% Input: Path of watermarked image
-%        Resize ratio. E.g: 2 mean scale x2
-% Output: Matrices of scaled image
+% Input: Host image path
+% Output: Best watermark size
 
-% Doc file anh
-I = imread(WatermarkedImagePath);
+% Read image
+HostImage = imread(HostImagePath);
 
 % Get size
-[WatermarkedImageRows WatermarkedImageColumns ColorBand] = size(I);
+[HostImageR, HostImageC, HostImageChannel] = size(HostImage);
 
-% Attack
-% We should turn off anti anliasing because it's may cause change to pixel
-% data
-% Nen tat khu rang cua (anti aliasing) de tranh du lieu bi tu dong thay doi
-AttackedWatermarkedImage = imresize(I, ratio, 'Antialiasing',false);
-AttackedWatermarkedImage = imresize(AttackedWatermarkedImage, [WatermarkedImageRows WatermarkedImageColumns], 'nearest','Antialiasing',false);
+% Calculate
+HostImageBlock = (HostImageR*HostImageC)/16;
+MaxBit = HostImageBlock * 4;
+MaxPixels = MaxBit / 8;
+PixelsLength = floor(sqrt(MaxPixels));
+
+% Output
+WatermarkH = PixelsLength;
+WatermarkW = PixelsLength;
 
 end
 
